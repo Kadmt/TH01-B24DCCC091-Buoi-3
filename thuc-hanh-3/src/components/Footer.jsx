c88aeeb5d3f0c3e75d92893d50ff3cab94c92a32
Kadmt <khonganhduy123@gmail.com>
2025-11-03 10:54:11 +0700

    Delete thuc-hanh-3 directory

--- thuc-hanh-3/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-    return (
-        <div>
-            <h1>This is footer </h1>
-        </div>
-    )
-}
--- thuc-hanh-3/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-    return (
-        <div>
-            <h1>Hello my name is Duy</h1>
-        </div>
-    )
-}
